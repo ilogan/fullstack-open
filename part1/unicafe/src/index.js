056cc2e3ae033fbd4e79bab485f1f356ce4be92e
Ian Logan <itlogan9@gmail.com>
2019-06-26 21:47:51 -0500

    Complete 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,6 +7,8 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const all = good + bad + neutral;
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -17,6 +19,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {(good * 1 + bad * -1) / all}</p>
+      <p>postive {(good / all) * 100}%</p>
     </div>
   );
 };
