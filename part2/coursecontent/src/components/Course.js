8c9355187493d9ccf09ca60e59a8e1eb17b7dbb9
Ian Logan <itlogan9@gmail.com>
2019-06-27 14:41:01 -0500

    Complete 2.5

--- part2/coursecontent/src/components/Course.js
+++ part2/coursecontent/src/components/Course.js
@@ -1,7 +1,32 @@
 import React from "react";
 
-import Header from "./Header";
-import Content from "./Content";
+const Header = ({ courseName }) => {
+  return <h2>{courseName}</h2>;
+};
+
+const Content = ({ parts }) => {
+  const partList = parts.map(part => (
+    <Part key={part.id} name={part.name} exercise={part.exercises} />
+  ));
+
+  const exerciseSum = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return (
+    <div>
+      {partList}
+      <p>
+        <strong>total of {exerciseSum} exercises</strong>
+      </p>
+    </div>
+  );
+};
+
+const Part = ({ name, exercise }) => {
+  return (
+    <p>
+      {name} {exercise}
+    </p>
+  );
+};
 
 const Course = ({ course }) => {
   return (
