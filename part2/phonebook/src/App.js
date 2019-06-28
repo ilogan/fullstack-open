cdbc2faf15c7f6a740d208b0bf603e497d798b53
Ian Logan <itlogan9@gmail.com>
2019-06-28 11:10:11 -0500

    Complete 2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,22 +1,40 @@
 import React, { useState } from "react";
 
 const App = () => {
+  // add button should add a new person to the list
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  // new name should be the name within form up until submit
   const [newName, setNewName] = useState("");
+
+  const handleChange = event => {
+    setNewName(event.target.value);
+  };
+
+  const handleClick = event => {
+    event.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+    setNewName("");
+  };
+
+  const personList = persons.map(person => (
+    <div key={person.name}>{person.name}</div>
+  ));
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleChange} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" onClick={handleClick}>
+            add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {personList}
     </div>
   );
 };
