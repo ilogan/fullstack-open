443a97007a3443d46f8dd6fc5658fdbe7f1e7b3b
Ian Logan <itlogan9@gmail.com>
2019-06-29 13:14:56 -0500

    Complete 2.14

--- part2/countries/src/components/CountryView.js
+++ part2/countries/src/components/CountryView.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const CountryView = ({ name, capital, population, languages, flag }) => {
+  const [weather, setWeather] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://api.apixu.com/v1/current.json?key=${
+          process.env.REACT_APP_API_KEY
+        }&q=${capital}`
+      )
+      .then(response => setWeather(response.data.current));
+  }, [capital]);
+
   const renderLanguages = () => {
     return languages.map(language => (
       <li key={language.iso639_2}>{language.name}</li>
     ));
   };
+
+  const weatherIcon = weather.condition ? weather.condition.icon : null;
 
   return (
     <div>
@@ -15,6 +30,15 @@ const CountryView = ({ name, capital, population, languages, flag }) => {
       <h2>languages</h2>
       <ul>{renderLanguages()}</ul>
       <img src={flag} style={{ width: "300px" }} alt="Country flag" />
+      <h2>Weather</h2>
+      <p>
+        <strong>temperature:</strong> {weather.temp_c} Celcius
+      </p>
+      <img src={weatherIcon} alt="Weather Icon" />
+      <p>
+        <strong>wind:</strong> {weather.wind_kph} kph direction{" "}
+        {weather.wind_dir}
+      </p>
     </div>
   );
 };
