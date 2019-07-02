7adf11e7b4846de0e0377bd6ac92c1edb9437b58
Ian Logan <itlogan9@gmail.com>
2019-07-02 16:37:15 -0500

    Initial Commit (phonebook-backend)

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+
+const persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4
+  }
+];
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
