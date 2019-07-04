892f2aa97d53a0a447a8353f72a11d8cc0b28b05
Ian Logan <itlogan9@gmail.com>
2019-07-04 11:02:54 -0500

    Add frontend

---
+++ build/precache-manifest.d60f27b2d144641c653020bf2d6f286a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a77db083f1dcac7a75f27f528e51e9cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57d6d3075c5ae76c89b6",
+    "url": "/static/css/main.6a943033.chunk.css"
+  },
+  {
+    "revision": "bc120ca277fc438795d3",
+    "url": "/static/js/2.dbf5eb8f.chunk.js"
+  },
+  {
+    "revision": "57d6d3075c5ae76c89b6",
+    "url": "/static/js/main.feaac96c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -27,6 +28,7 @@ let notes = [
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static("build"));
 
 const requestLogger = (req, res, next) => {
   console.log("Method: ", req.method);
