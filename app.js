fb3c2e7e546af4ae10b41cf40499e9d2edfd536d
Ian Logan <itlogan9@gmail.com>
2019-07-12 20:23:43 -0500

    Complete 4.2 - backend

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const blogRouter = require("./controllers/blog");
+const cors = require("cors");
+
+const app = express();
+
+console.log("connecting to", config.MONGODB_URL);
+
+mongoose
+  .connect(config.MONGODB_URL, { useNewUrlParser: true })
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch(error => {
+    console.log("error connecting to MongoDB", error.message);
+  });
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/blogs", blogRouter);
+
+module.exports = app;
