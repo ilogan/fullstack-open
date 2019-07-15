c5fe46e6b0d44db0a8a6ecc15110ed67a97e106f
Ian Logan <itlogan9@gmail.com>
2019-07-15 14:51:44 -0500

    Complete 4.5 - backend

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -9,7 +9,31 @@ const totalLikes = blogs => {
   return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
 };
 
+const favoriteBlog = blogs => {
+  let maxLikes = -1;
+  let favoriteIndex = null;
+
+  // find blog with most likes
+  for (let [i, blog] of blogs.entries()) {
+    if (blog.likes > maxLikes) {
+      maxLikes = blog.likes;
+      favoriteIndex = i;
+    }
+  }
+  // return blog if exists
+  if (maxLikes !== -1) {
+    const favoriteBlog = blogs[favoriteIndex];
+    return {
+      title: favoriteBlog.title,
+      author: favoriteBlog.author,
+      likes: favoriteBlog.likes
+    };
+  }
+  // return empty object if null
+  return {};
+};
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 };
