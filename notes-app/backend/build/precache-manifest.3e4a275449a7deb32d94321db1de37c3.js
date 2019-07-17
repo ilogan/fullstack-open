e4513588b8487a93652dbbc39ba487254c057d3d
Ian Logan <itlogan9@gmail.com>
2019-07-16 19:41:26 -0500

    Add notes-app backend subtree

---
+++ notes-app/backend/build/precache-manifest.3e4a275449a7deb32d94321db1de37c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "385307705645b886b82f32c73ea17ab3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "727b135a775b7781e642",
+    "url": "/static/css/main.6a943033.chunk.css"
+  },
+  {
+    "revision": "bc120ca277fc438795d3",
+    "url": "/static/js/2.dbf5eb8f.chunk.js"
+  },
+  {
+    "revision": "727b135a775b7781e642",
+    "url": "/static/js/main.287ae436.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
