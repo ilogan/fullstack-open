d6a629e5bb580d3199b8d2f5c3a87ec8f4436890
Ian Logan <itlogan9@gmail.com>
2019-07-18 19:38:54 -0500

    Complete 4.8 - backend

--- utils/config.js
+++ utils/config.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 
-const MONGODB_URL = process.env.MONGODB_URL;
-const PORT = process.env.PORT;
+let PORT = process.env.PORT;
+let MONGODB_URL = process.env.MONGODB_URL;
+
+if (process.env.NODE_ENV === "test") {
+  MONGODB_URL = process.env.TEST_MONGODB_URL;
+}
 
 module.exports = {
-  MONGODB_URL,
-  PORT
+  PORT,
+  MONGODB_URL
 };
